e2ca6d2ba66868650c2dbed3030817a1bf64ec4a
Mohammed Ahmed <mohammedabdalla38@yahoo.com>
2020-08-15 17:56:56 -0500

    washing freq data

--- app.js
+++ app.js
@@ -3,4 +3,33 @@ function buildPlot(id) {
     // get data from the json using d3
     d3.json("Data/samples.json").then((data)=> {
         console.log(data)
+
+    // get washing frequency data
+    var wfreq = data.metadata.map(d => d.wfreq)
+        console.log(`Washing Freq: ${wfreq}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
