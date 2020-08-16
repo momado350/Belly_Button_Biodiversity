4f4882b5196cf9442d2948323cfc2f1149c8bc3b
Mohammed Ahmed <mohammedabdalla38@yahoo.com>
2020-08-15 21:08:35 -0500

    create the function for the initial data rendering

--- app.js
+++ app.js
@@ -154,6 +154,25 @@ function optionChanged(id) {
     buildInfo(id);
 }
     
+    // create the function for the initial data rendering
+function init() {
+    // select dropdown menu 
+    var dropdown = d3.select("#selDataset");
+
+    // read the data 
+    d3.json("Data/samples.json").then((data)=> {
+        console.log(data)
+
+        // get the id data to the dropdwown menu
+        data.names.forEach(function(name) {
+            dropdown.append("option").text(name).property("value");
+        });
+
+        // call the functions to display the data and the plots to the page
+        buildPlot(data.names[0]);
+        buildInfo(data.names[0]);
+    });
+}
 
 
 
